0d90f7ab10e4b18f3ca5bb6eb5e1714363f83859
tahir.aslam.cytena <tahir_aslam786@yahoo.com>
2022-06-11 18:24:40 +0500

    feat: extension methods

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "I am thankfull to you for your services";
+            var shortenPost = post.Shorten(5);
+            Console.WriteLine(post);
+            Console.WriteLine(shortenPost+" ...");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExtensionMethods/StringEntensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    public static class StringEntensions
+    {
+        public static string Shorten(this string str, int noOfWords)
+        {
+            if (noOfWords < 0)
+                throw new ArgumentOutOfRangeException("no of words should not be less than zero");
+
+            var words = str.Split(' ');
+
+            if (words.Length < noOfWords)
+                return str;
+
+            return string.Join(" ", words.Take(noOfWords));
+        }
+    }
+}
