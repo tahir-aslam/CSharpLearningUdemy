50d6c2b34375d9ee354d4b76c491affd60f4478f
tahir.aslam.cytena <tahir_aslam786@yahoo.com>
2022-06-12 15:14:39 +0500

    feat: Nullable types

---
+++ NullableTypes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nullable<DateTime> dateTime = null;
+            DateTime? dateTime = null;
+
+            Console.WriteLine("GetValueOrDefault: " + dateTime.GetValueOrDefault());
+            Console.WriteLine("HasValue: " + dateTime.HasValue);
+            Console.WriteLine("Value: " + dateTime.Value);
+
+            DateTime? date = new DateTime(2022, 06, 12);
+            DateTime date2 = date.GetValueOrDefault();
+
+            //null coalscing operator   
+            DateTime? date3 = null;
+            DateTime date4 = date3 ?? DateTime.Today;
+
+            Console.ReadKey();
+
+        }
+    }
+}
