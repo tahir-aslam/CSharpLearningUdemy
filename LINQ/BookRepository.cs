6de4a76be1728b116991cdc83eac8596573a48a3
tahir.aslam.cytena <tahir_aslam786@yahoo.com>
2022-06-12 14:41:00 +0500

    feat: Linq extension methods

---
+++ LINQ/BookRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace LINQ
+{
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBooks()
+        {
+            return new List<Book>
+            {
+                new Book(){ Title ="ASP .Net", Price=85.5f },
+                new Book(){ Title="WPF MVVM", Price=80.5f},
+            };
+        }
+    }
+}
---
+++ LINQ/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var books = new BookRepository().GetBooks();
+
+            //Linq query operator
+            var cheaperBooks = from b in books
+                               where b.Price < 100
+                               orderby b.Title
+                               select b;
+
+            //Linq extension methods
+            var cheapBooks = books
+                .Where(b => b.Price < 100)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title);
+
+            var paging = cheapBooks.Skip(2).Take(5);
+        }
+    }
+}
