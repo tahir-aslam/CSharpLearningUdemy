3e54b3063117571930f778e0a2ea6366eaa34310
tahir.aslam.cytena <tahir_aslam786@yahoo.com>
2022-06-12 15:53:04 +0500

    feat: dynamics

---
+++ Dynamics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = "Tahir";
+            var hashCode = obj.GetHashCode();
+            var type = obj.GetType();
+
+            //var excelObject = "Aslam";
+            dynamic excelObject = "Aslam";
+            excelObject.Optimize();
+
+            dynamic name = "Tahir"; //string
+            name = 10; // int
+
+            Console.Read();
+        }
+    }
+}
